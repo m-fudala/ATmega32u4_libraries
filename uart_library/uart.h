2e15bddb8107e98c68a6f09c19dd2732a08bc46d
Michał Fudała <michfudala@gmail.com>
2024-02-11 13:39:19 +0100

    Prototype of UART library

---
+++ uart_library/uart.h
@@ -0,0 +1,15 @@
+/*
+    Library handling Universal asynchronous receiver-transmitter of the
+    ATmega32u4. https://www.microchip.com/en-us/product/atmega32u4
+
+    Author: https://github.com/m-fudala
+*/
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <avr/io.h>
+
+
+
+#endif
---
+++ uart_library/uart_example.c
@@ -0,0 +1,18 @@
+/*
+    Example of using UART library.
+
+    https://github.com/m-fudala
+*/
+
+#include "uart.h"
+
+int main()
+{
+    
+
+    while (1) {
+        
+    }
+
+    return 0;
+}
