00a0f318f33c6485ba7e1770ae0b4726f4afbe6c
Michał Fudała <michfudala@gmail.com>
2024-02-20 13:14:04 +0100

    Remaining interrupts implemented

--- external_interrupt_library/external_interrupt.c
+++ external_interrupt_library/external_interrupt.c
@@ -1,5 +1,93 @@
 #include "external_interrupt.h"
 
+void int0_init(void (*int_function)(void), unsigned char int_level) {
+    cli();
+
+    int0_function_pointer = int_function;
+
+    EICRA |= int_level;
+
+    sei();
+}
+
+void int0_enable() {
+    EIMSK |= _BV(INT0);
+}
+
+void int0_disable() {
+    EIMSK &= ~_BV(INT0); 
+}
+
+ISR (INT0_vect) {
+    int0_function_pointer();
+}
+
+void int1_init(void (*int_function)(void), unsigned char int_level) {
+    cli();
+
+    int1_function_pointer = int_function;
+
+    EICRA |= int_level << 2;
+
+    sei();
+}
+
+void int1_enable() {
+    EIMSK |= _BV(INT1);
+}
+
+void int1_disable() {
+    EIMSK &= ~_BV(INT1); 
+}
+
+ISR (INT1_vect) {
+    int1_function_pointer();
+}
+
+void int2_init(void (*int_function)(void), unsigned char int_level) {
+    cli();
+
+    int2_function_pointer = int_function;
+
+    EICRA |= int_level << 4;
+
+    sei();
+}
+
+void int2_enable() {
+    EIMSK |= _BV(INT2);
+}
+
+void int2_disable() {
+    EIMSK &= ~_BV(INT2); 
+}
+
+ISR (INT2_vect) {
+    int2_function_pointer();
+}
+
+void int3_init(void (*int_function)(void), unsigned char int_level) {
+    cli();
+
+    int3_function_pointer = int_function;
+
+    EICRA |= int_level << 6;
+
+    sei();
+}
+
+void int3_enable() {
+    EIMSK |= _BV(INT3);
+}
+
+void int3_disable() {
+    EIMSK &= ~_BV(INT3); 
+}
+
+ISR (INT3_vect) {
+    int3_function_pointer();
+}
+
 void int6_init(void (*int_function)(void), unsigned char int_level) {
     cli();
 
@@ -19,6 +107,5 @@ void int6_disable() {
 }
 
 ISR (INT6_vect) {
-    // int6_function_pointer();
-    PINC |= _BV(PC6);
+    int6_function_pointer();
 }
--- external_interrupt_library/external_interrupt.h
+++ external_interrupt_library/external_interrupt.h
@@ -11,19 +11,52 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-enum Interrupt_level {
+enum Interrupt_level {  // enum holding possible interrupt levels
     LOW_LEVEL,
     ANY_EDGE,
     FALLING_EDGE,
     RISING_EDGE
 };
 
+// pointers to functions to be called in specified interrupt handlers
+void (*int0_function_pointer)(void);
+void (*int1_function_pointer)(void);
+void (*int2_function_pointer)(void);
+void (*int3_function_pointer)(void);
 void (*int6_function_pointer)(void);
 
+/*
+    Functions initializing external interrupts
+
+    Parameters:
+        int_function - pointer to function that is to be called in ISR
+        int_level - value from enum Interrupt_level
+*/
+
+void int0_init(void (*int_function)(void), unsigned char int_level);
+void int1_init(void (*int_function)(void), unsigned char int_level);
+void int2_init(void (*int_function)(void), unsigned char int_level);
+void int3_init(void (*int_function)(void), unsigned char int_level);
 void int6_init(void (*int_function)(void), unsigned char int_level);
 
+/*
+    Functions enabling specified interrupt
+*/
+
+void int0_enable();
+void int1_enable();
+void int2_enable();
+void int3_enable();
 void int6_enable();
 
+/*
+    Functions disabling specified interrupt
+*/
+
+void int0_disable();
+void int1_disable();
+void int2_disable();
+void int3_disable();
 void int6_disable();
 
 #endif
